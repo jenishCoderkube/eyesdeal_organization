ef49c4d5a92449db8e44009e3545519febbd28de
Dhruvin-41-ck <dhruvin.coderkube@gmail.com>
2025-05-02 10:44:44 +0530

    issus fixed

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -60,8 +60,9 @@ import GstReport from "../pages/Reports/GstReport";
 import ProductInventoryReport from "../pages/Reports/ProductInventoryReport";
 import MediaLibrary from "../pages/MediaLibrary/MediaLibrary";
 import SubFolder from "../pages/MediaLibrary/SubFolder";
-import VendorListProcess from "../pages/Process/vendorListProcess";
+
 import ExportProducts from "../pages/Products/ExportProducts";
+import VendorListProcess from "../pages/Process/vendorListProcess";
 
 const routes = [
   {
