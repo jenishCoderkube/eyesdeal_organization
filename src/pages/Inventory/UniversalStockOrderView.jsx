d6b6c5ac6aaf3ebaf5d2a5219fa03a84880ba868
Dhruvi2309 <dhruvi.coderkube@gmail.com>
2025-10-08 17:10:50 +0530

    universal stock order

---
+++ src/pages/Inventory/UniversalStockOrderView.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UniversalStockOrderCom from "../../components/Inventory/Stock/UniversalStockOrderCom";
+
+const UniversalStockOrderView = () => {
+  return (
+    <div>
+      <UniversalStockOrderCom />
+    </div>
+  );
+};
+
+export default UniversalStockOrderView;
